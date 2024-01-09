ef42d8fb53dc9e6436707462ee752132cbfabbca
Shraddha Ranjan <ranjanshraddha@yahoo.co.uk>
2024-01-09 16:27:03 +0000

    renamed files

---
+++ index.js
@@ -0,0 +1,15 @@
+function onClickButton(countryId) {
+    if (countryId) {
+        if (countryId === "australiaButton") {
+            window.location.href = "australia.html";
+        } else if (countryId === "japanButton") {
+            window.location.href = "japan.html";
+        } else if (countryId === "finlandButton") {
+            window.location.href = "finland.html";
+        } else if (countryId === "switzerlandButton") {
+            window.location.href = "switzerland.html";
+        } else {
+            console.log("Button not found");
+        }
+    }
+}
